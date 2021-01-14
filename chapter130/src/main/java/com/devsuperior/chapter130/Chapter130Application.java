eb1ab6c32bd44a1dd3f739800367cbe8a01b02fc
Demian Tatarcenkas Diniz <demian.dinizdd@campus.mccd.edu>
2021-01-14 14:23:17 -0300

    Project created

---
+++ chapter130/src/main/java/com/devsuperior/chapter130/Chapter130Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.chapter130;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter130Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter130Application.class, args);
+	}
+
+}
---
+++ chapter130/src/test/java/com/devsuperior/chapter130/Chapter130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.chapter130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter130ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
