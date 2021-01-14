22d3a06649dafad879cc16d055c5660facd02f05
Demian Tatarcenkas Diniz <demian.dinizdd@campus.mccd.edu>
2021-01-14 18:55:56 -0300

    Post new, update, delete Client

--- chapter130/src/main/java/com/devsuperior/chapter130/resources/ClientResource.java
+++ chapter130/src/main/java/com/devsuperior/chapter130/resources/ClientResource.java
@@ -1,13 +1,19 @@
 package com.devsuperior.chapter130.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.chapter130.DTO.ClientDTO;
 import com.devsuperior.chapter130.entities.Client;
@@ -32,4 +38,26 @@ public class ClientResource  {
 
 		return ResponseEntity.ok().body(dto);
 	}
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
