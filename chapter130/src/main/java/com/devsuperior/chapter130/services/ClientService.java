d285759e3774690af3db01fe514f55bff1704ade
Demian Tatarcenkas Diniz <demian.dinizdd@campus.mccd.edu>
2021-01-14 17:24:43 -0300

    Database seeding

--- chapter130/src/main/java/com/devsuperior/chapter130/services/ClientService.java
+++ chapter130/src/main/java/com/devsuperior/chapter130/services/ClientService.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.chapter130.repositories.ClientRepository;
 import com.devsuperior.chapter130.entities.Client;
+import com.devsuperior.chapter130.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> findAll() {
 		return repository.findAll();
 	}
